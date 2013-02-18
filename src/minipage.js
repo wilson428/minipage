9d6ef8f140a8b9a68c1630fc6173a17e5650d190
Chris Wilson <christopher.e.wilson@gmail.com>
2013-02-18 11:18:48 -0500

    Wordcount now uses jQ

--- src/minipage.js
+++ src/minipage.js
@@ -8,16 +8,34 @@ var walk_the_DOM = function walk(node, func) {
     }
 };
 
-var wordcount = function(node) {
+var wordcount = function(node, all) {
     var count = 0;
-    walk_the_DOM(node, function(node) {
-        if (node.nodeType === 3) {
-            var words = node.nodeValue.match(/[A-z']+/g);
-            if (words) {
-                count += words.length;
-            }            
-        }
-    });
+    if (all) {
+        /*
+        walk_the_DOM(node, function(node) {
+            if (node.nodeType === 3) {
+                var words = node.nodeValue.match(/[A-z']+/g);
+                if (words) {
+                    count += words.length;
+                }            
+            }
+        });
+        */
+        var words = $(node).text().match(/[A-z']+/g);
+        if (words) {
+            count += words.length;
+        }            
+    } else {
+        $(node).contents().each(function(i, v) {
+            if (v.nodeType === 3) {
+                var words = $(node).text().match(/[A-z']+/g);
+                if (words) {
+                    count += words.length;
+                }        
+            }  
+        });
+        
+    }
     return count;
 };
 
@@ -27,42 +45,49 @@ var minipage = function(opts) {
     if (!opts) { opts = {}; }
     if (!opts.target) {
         opts.target = document;
+    } else if (typeof opts.target === "string") {
+        if (opts.target[0] !== "#" && opts.target[0] !== ".") {
+            opts.target = "#" + opts.target;
+        }
+        opts.target = $(opts.target)[0];
     }
+    
     if (!opts.container) {
         opts.container = document.createElement("DIV");
         opts.container.className = "mini-container";
         document.body.appendChild(opts.container);
-    } else if (typeof opts.container === "String") {
-        if (opts.container[0] !== "#") {
+    } else if (typeof opts.container === "string") {
+        if (opts.container[0] !== "#" && opts.container[0] !== ".") {
             opts.container = "#" + opts.container;
         }
-        opts.container = $(opts.container);
+        opts.container = $(opts.container)[0];
     }
         
     // it's a good idea to to build the minipage off the DOM first, so that the walk_to_DOM function doesn't crawl the nodes it creates even though it would ignore them
     var holder = document.createElement("DIV");
-
+    $(holder).css({
+        height: $(opts.target).height()
+    });
     //nodes to draw
     var whitelist = {
         text: opts.textnodes || ["p", "li"],
         elements: opts.elementnodes || ["img"]
     };
-        
-    var xscale = $(opts.container).width() / $(opts.target).width(),
-        yscale = $(opts.container).height() / $(opts.target).height(),
+    
+    var xscale = ($(opts.container).width() - (opts.margin * 2 || 10)) / $(opts.target).width(),
+        yscale = ($(opts.container).height() - (opts.margin * 2 || 10)) / $(opts.target).height(),
         paper = Raphael(holder),
         shapes = paper.set();
 
     function draw(node) {
         //see if we have a text node (nodeType === 3) that is not blank        
         var tag = node.tagName ? node.tagName.toLowerCase() : "";
-        if (whitelist.text.indexOf(tag) > -1 && wordcount(node) > 0 || whitelist.elements.indexOf(tag) > -1) {
-            //console.log(node);
+        if (whitelist.text.indexOf(tag) > -1 && wordcount(node, 0) > 0 || whitelist.elements.indexOf(tag) > -1) {
             var h = node.offsetHeight * yscale,
                 w = node.offsetWidth * xscale,
-                top = $(node).offset().top * yscale,
-                left = $(node).offset().left * xscale; 
-                
+                top = ($(node).position().top - $("#complete").position().top) * yscale + opts.margin,
+                left = $(node).position().left * xscale; 
+            console.log(node, $(node).position(), $(node).offset());
             var rect = paper.rect(left, top, w, h);
             rect.node.setAttribute("class", "mini-" + tag + " mini-" + (whitelist.text.indexOf(tag) > -1 ? "text" : "element"));
             rect.sibling = node;
@@ -78,6 +103,18 @@ var minipage = function(opts) {
     
     make();
 
+    //shift up to parent offset
+    //shapes.transform("T0,-" + $(opts.container).offset().top);
+
+    shapes.hover(
+        function() {
+            this.sibling.original = this.sibling.style.backgroundColor;
+            this.sibling.style.backgroundColor = "red";
+        },
+        function() {
+            this.sibling.style.backgroundColor = this.sibling.original;
+        });
+        
     return {
         make: function() {
             make();
